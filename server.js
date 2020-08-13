111b0cfb65749e53609b4bc4c17cb49f45829606
Jeff Mead <jeffdmead171@gmail.com>
2020-08-13 14:27:49 -0400

    updted apiRoutes and htmlRoutes

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 
 const { notes } = require('./db/db');
 
@@ -35,31 +44,8 @@ function validateNote(note) {
   return true;
 }
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-  });
 
-app.get('/api/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-});
 
-app.post('/api/notes', (req, res) => {
-  // set id based on what the next index of the array will be
-  req.body.id = notes.length.toString();
-
-  // add animal to json file and animals array in this function
-  if (!validateNote(req.body)) {
-    res.status(400).send('The note is not properly formatted.');
-  } else {
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-  }
-});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
