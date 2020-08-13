2a864e4d47bc767409cbb2525cbc9df6de5cbd12
Jeff Mead <jeffdmead171@gmail.com>
2020-08-13 13:26:58 -0400

    added GET and POST

--- server.js
+++ server.js
@@ -1,13 +1,65 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const { notes } = require('./db/db');
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+  }
 
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+
+  return note;
+}
+
+function validateNote(note) {
+  if (!note.title || typeof note.title !== 'string') {
+    return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+    return false;
+  }
+  return true;
+}
 
 app.get('/api/notes', (req, res) => {
-    res.send('Hello!');
+    res.json(notes);
   });
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+app.post('/api/notes', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  // add animal to json file and animals array in this function
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
